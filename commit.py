2ccc4bed2021169a3c9b3e32ddf4cd8db53705c1
ilmir-muslim <ilmir_gf@mail.ru>
2022-05-12 13:00:08 +0200

    1

---
+++ commit.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def run(*args):
+
+    return subprocess.check_call(['git'] + list(args))
+
+def commit():
+
+    message = input("\nType in your commit message: ")
+    commit_message = f'{message}'
+
+    run("commit", "-am", commit_message)
+    run("push", "-u", "origin", "master")
+commit()
